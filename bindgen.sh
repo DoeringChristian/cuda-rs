8769d57807a51c2a3c15c8eb802ca8909e2d67a2
Döring Christian <christian.doering@um.de>
2023-04-12 11:28:42 +0200

    Initial Commint (Fork from Rust-CUDA/cust_raw)

---
+++ bindgen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -exu
+
+bindgen \
+  --allowlist-type="^CU.*" \
+  --allowlist-type="^cuuint(32|64)_t" \
+  --allowlist-type="^cudaError_enum" \
+  --allowlist-type="^cu.*Complex$" \
+  --allowlist-type="^cuda.*" \
+  --allowlist-type="^libraryPropertyType.*" \
+  --allowlist-var="^CU.*" \
+  --allowlist-function="^cu.*" \
+  --default-enum-style=rust \
+  --no-doc-comments \
+  --with-derive-default \
+  --with-derive-eq \
+  --with-derive-hash \
+  --with-derive-ord \
+  wrapper.h -- -I/opt/cuda/include \
+  > src/cuda.rs
